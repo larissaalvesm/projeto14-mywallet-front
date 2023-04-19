92cfe37cd0eeccdcfd32b5a8b9cc3d5adb8e64db
larissa <larissaalves1102@gmail.com>
2023-04-19 13:34:43 -0300

    váriavel de ambiente REACT_APP_API_URL adicionada

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -10,42 +10,42 @@ export default function SignUpPage() {
   const [nome, setNome] = useState("");
   const [senha, setSenha] = useState("");
   const [confSenha, setConfSenha] = useState("");
-  const [disabled, setDisabled] = useState(false);
   const navigate = useNavigate();
+  const baseURL = process.env.REACT_APP_API_URL;
+  const api = axios.create({
+    baseURL: baseURL
+  })
 
   function cadastrar(e) {
     e.preventDefault();
-    setDisabled(true);
 
     if (senha !== confSenha) {
       alert("Senhas diferentes.");
-      setDisabled(false);
     } else {
       const obj = {
         email,
         nome,
         senha
       }
-      const request = axios.post("http://localhost:5000/cadastro", obj);
+      const request = api.post("/cadastro", obj);
 
       request.then(() => navigate("/"));
 
       request.catch(err => {
         alert(err.response.data);
-        setDisabled(false);
       });
     }
 
   }
   return (
     <SingUpContainer>
-      <form disabled={disabled} onSubmit={cadastrar}>
+      <form onSubmit={cadastrar}>
         <MyWalletLogo />
         <input data-test="name" placeholder="Nome" type="text" required value={nome} onChange={e => setNome(e.target.value)} />
         <input data-test="email" placeholder="E-mail" type="email" required value={email} onChange={e => setEmail(e.target.value)} />
         <input data-test="password" placeholder="Senha" type="password" required value={senha} onChange={e => setSenha(e.target.value)} />
         <input data-test="conf-password" placeholder="Confirme a senha" type="password" autocomplete="new-password" required value={confSenha} onChange={e => setConfSenha(e.target.value)} />
-        <button data-test="sign-up-submit" disabled={disabled} type="submit">Cadastrar</button>
+        <button data-test="sign-up-submit" type="submit">Cadastrar</button>
       </form>
 
       <Link to="/">
